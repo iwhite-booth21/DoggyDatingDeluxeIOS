e893af64cddf2ab3c4eaa112ea74c4fadc69fd22
iwhite-booth21 <40900135+iwhite-booth21@users.noreply.github.com>
2022-03-16 02:50:09 -0500

    New Update

---
+++ DoggyDatingDeluxeIOS/SignUpViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SignUpViewController.swift
+//  DoggyDatingDeluxeIOS
+//
+//  Created by Isaiah White-Booth on 3/16/22.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onFinalSignUpBtn(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.email = emailField.text
+        
+        user.signUpInBackground{(success, error) in
+            if success{
+                self.performSegue(withIdentifier: "userSignUpSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
