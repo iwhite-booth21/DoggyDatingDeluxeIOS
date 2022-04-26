7d6d6117217c23b0a408f950cddf4e11dec19f88
iwhite-booth21 <40900135+iwhite-booth21@users.noreply.github.com>
2022-04-25 22:26:37 -0500

    New Screens Added, Account Update and Accompanying Screens

---
+++ DoggyDatingDeluxeIOS/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  DoggyDatingDeluxeIOS
+//
+//  Created by Isaiah White-Booth on 4/21/22.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DoggyDatingDeluxeIOS/UpdateProfileViewController.swift
@@ -0,0 +1,48 @@
+//
+//  UpdateProfileViewController.swift
+//  DoggyDatingDeluxeIOS
+//
+//  Created by Isaiah White-Booth on 4/22/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class UpdateProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var updUserField: UITextField!
+    
+    @IBOutlet weak var updPetField: UITextField!
+    
+    @IBOutlet weak var updBioField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onUpdBtn(_ sender: Any) {
+            
+        
+        
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
